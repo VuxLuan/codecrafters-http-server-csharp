e866fb7d5ac32af18d39ccf867809b6f0b5302e1
VuxLuan <luanvu@MBL.local>
2024-11-04 09:14:26 +0700

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,6 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+socket.Send("HTTP/1.1 200 OK\r\n\r\n"u8.ToArray());
+
